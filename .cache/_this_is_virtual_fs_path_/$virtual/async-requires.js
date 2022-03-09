8a914a80d48a22bbb935dd7c5a4ea3b4b570070f
shampien <shamsuri.azmi@gmail.com>
2022-03-10 01:54:47 +0800

    deleted

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,5 +0,0 @@
-exports.components = {
-  "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": () => import("./../../../node_modules/@wkocjan/gatsby-theme-intro/src/pages/404.js" /* webpackChunkName: "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js" */),
-  "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js": () => import("./../../../node_modules/@wkocjan/gatsby-theme-intro/src/templates/index.js" /* webpackChunkName: "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js" */)
-}
-
--- README.md
+++
@@ -1,1 +0,0 @@
-# shamsuriazmi.github.io

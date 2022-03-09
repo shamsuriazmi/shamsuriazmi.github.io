45037748662f5d41eee0eaff7066f2856dda4fc5
shampien <shamsuri.azmi@gmail.com>
2022-03-10 01:45:59 +0800

    2nd commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\PC\\Documents\\GitHub\\shamsuriazmi.github.io\\my-site\\.cache\\dev-404-page.js")),
   "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": preferDefault(require("C:\\Users\\PC\\Documents\\GitHub\\shamsuriazmi.github.io\\my-site\\node_modules\\@wkocjan\\gatsby-theme-intro\\src\\pages\\404.js")),
   "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js": preferDefault(require("C:\\Users\\PC\\Documents\\GitHub\\shamsuriazmi.github.io\\my-site\\node_modules\\@wkocjan\\gatsby-theme-intro\\src\\templates\\index.js"))
 }
